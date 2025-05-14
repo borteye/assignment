2e4b2fe447f14c5d16716a262b75874235e1dceb
borteye <gabrielborteye12199@gmail.com>
2025-05-14 15:50:15 +0000

    feat: add Contact Us component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import About from "./components/about";
+import ContactUs from "./components/contact-us/contact-us";
 import Hero from "./components/hero";
 
 export default function Home() {
   return (
     <div>
       <Hero />
-      <About/>
+      <About />
+      <ContactUs />
     </div>
   );
 }
