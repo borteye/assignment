0a1ec33b36a1c473075b0d255a2a4f9a2bfc4e20
borteye <gabrielborteye12199@gmail.com>
2025-05-14 20:08:50 +0000

    feat: replace logo section with Logo component for improved structure

--- src/app/components/layout/nav-bar.tsx
+++ src/app/components/layout/nav-bar.tsx
@@ -4,22 +4,12 @@ import styles from "@/app/styles/nav-bar.module.css";
 import { Menu } from "lucide-react";
 import Link from "next/link";
 import Button from "../shared/button";
+import Logo from "../shared/logo";
 
 export default function NavBar() {
   return (
     <nav className={styles.navContainer}>
-      <div className={styles.navLogoContainer}>
-        <Image
-          src="/images/globe.png"
-          alt="Globe"
-          width={24}
-          height={24}
-          priority
-          quality={100}
-          className={styles.navLogo}
-        />
-        <h1 className={styles.navLogoText}>HELLO WORLD</h1>
-      </div>
+      <Logo />
 
       <div className={`${styles.navLinks} `}>
         {navLinks.map((link, index) => (
