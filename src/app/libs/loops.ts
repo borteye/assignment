71ccdaf2fd0d98c205dfce6e702434ddea1153f0
borteye <gabrielborteye12199@gmail.com>
2025-05-15 15:30:42 +0000

    feat: add blog data structure with sample entries for dynamic rendering

--- src/app/libs/loops.ts
+++ src/app/libs/loops.ts
@@ -1,4 +1,5 @@
 import { Youtube, Instagram, Twitter } from "lucide-react";
+import blogImage from "@/../public/images/blog_img.png";
 
 export const navLinks = [
   {
@@ -31,5 +32,29 @@ export const socials = [
   {
     icon: Twitter,
     link: "#",
+  },
+];
+
+export const blogs = [
+  {
+    id: 1,
+    image: blogImage,
+    title: "How to create a navigation bar",
+    description:
+      "navigation bar consist of your web logo, navigation links and action buttons, however, you can create unique navigations based on your website genre",
+  },
+  {
+    id: 2,
+    image: blogImage,
+    title: "How to create a navigation bar",
+    description:
+      "navigation bar consist of your web logo, navigation links and action buttons, however, you can create unique navigations based on your website genre",
+  },
+  {
+    id: 3,
+    image: blogImage,
+    title: "How to create a navigation bar",
+    description:
+      "navigation bar consist of your web logo, navigation links and action buttons, however, you can create unique navigations based on your website genre",
   },
 ];
